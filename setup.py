4440a655c2a280cac5091948d228560d296cbefb
Bharath.R <bharathr27097@gmail.com>
2024-12-02 12:52:07 +0530

    File Update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Shipmentpricepredictor',
+    packages=find_packages(),
+    version='0.1.0',
+    author='Bharath R',
+    author_email='bharathr27097@gmail.com',
+    
+)
